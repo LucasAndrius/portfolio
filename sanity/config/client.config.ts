ecc40f8ccd30f25475dfbfc098d71d579426147a
unknown <lukasandrios@gmail.com>
2023-11-17 16:51:25 -0300

    mod useCdn

--- sanity/config/client.config.ts
+++ sanity/config/client.config.ts
@@ -2,6 +2,7 @@ const config = {
   projectId: process.env.SANITY_PROJECTID,
   dataset: process.env.SANITY_DATASET,
   apiVersion: process.env.SANITY_API_VERSION,
+  useCdn: false,
 };
 
 export default config;
